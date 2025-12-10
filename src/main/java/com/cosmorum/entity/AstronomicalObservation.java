0aed8681952b410a87a7b2cace7935e7be2835b5
Olesia Shevchenko <olesia.shevchenko245@gmail.com>
2025-12-10 15:33:13 +0200

    Update AstronomicalObservation.java

--- src/main/java/com/cosmorum/entity/AstronomicalObservation.java
+++ src/main/java/com/cosmorum/entity/AstronomicalObservation.java
@@ -12,7 +12,7 @@ public class AstronomicalObservation {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String name;
 
     @Column(columnDefinition = "TEXT")
